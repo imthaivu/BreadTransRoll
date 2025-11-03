462e00abba9522b513a14c4bbc0ae80b81562150
imthaivu <imquocthai@gmail.com>
2025-11-03 23:46:42 +0700

    sgk

--- src/app/(routes)/(features)/classes/teacher/page.tsx
+++ src/app/(routes)/(features)/classes/teacher/page.tsx
@@ -3,19 +3,31 @@
 import { RequireAuth, RequireRole } from "@/lib/auth/guard";
 import { UserRole } from "@/lib/auth/types";
 import { TeacherClassesList } from "@/modules/classes/components/ClassesList";
-
+import Image from "next/image";
 export default function TeacherClassesPage() {
   return (
     <RequireAuth>
       <RequireRole roles={[UserRole.TEACHER]}>
         <main>
-            <div className="text-center pt-4 mb-4 sm:pt-8 sm:mb-8">
-              <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-gray-800 mb-2 sm:mb-4">
-                Lớp học của tôi
-              </h1>
-              <a className="text-2xl" href="https://imthaivu.github.io/sgkTiengAnh/" target="_blank" rel="noopener noreferrer">Sách giáo khoa <img src={"/assets/images/ebook.png"} alt="" /></a>
-            </div>
-
+          <div className="text-center pt-4 mb-4 sm:pt-8 sm:mb-8">
+            <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-gray-800 mb-2 sm:mb-4">
+              Lớp học của tôi
+            </h1>
+          </div>
+          <a
+            href="https://imthaivu.github.io/sgkTiengAnh/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sách giáo khoa
+            <Image
+              src="/assets/images/ebook.png"
+              alt="Ebook"
+              width={24}
+              height={24}
+              className="inline-block ml-1"
+            />
+          </a>
           <TeacherClassesList />
         </main>
       </RequireRole>
