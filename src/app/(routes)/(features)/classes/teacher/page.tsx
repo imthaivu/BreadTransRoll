32bfff6efc951e1c76758adcea722043bf62211c
imthaivu <imquocthai@gmail.com>
2025-11-15 07:13:08 +0700

    del sgk

--- src/app/(routes)/(features)/classes/teacher/page.tsx
+++ src/app/(routes)/(features)/classes/teacher/page.tsx
@@ -3,7 +3,6 @@
 import { RequireAuth, RequireRole } from "@/lib/auth/guard";
 import { UserRole } from "@/lib/auth/types";
 import { TeacherClassesList } from "@/modules/classes/components/ClassesList";
-import Image from "next/image";
 export default function TeacherClassesPage() {
   return (
     <RequireAuth>
@@ -14,22 +13,6 @@ export default function TeacherClassesPage() {
               Lớp học của tôi
             </h1>
           </div>
-          <a
-            href="https://imthaivu.github.io/sgkTiengAnh/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {/* căn giữa */}
-            <div className="flex justify-center items-center">
-              <Image
-                src="/assets/images/ebook.png"
-                alt="Ebook"
-                width={80}
-                height={80}
-                className="inline-block ml-1 h-20 w-auto"
-              />
-            </div>
-          </a>
           <TeacherClassesList />
         </main>
       </RequireRole>
