0287fb5568d26e6b241b280d6189f84680df3a06
imthaivu <imquocthai@gmail.com>
2025-11-15 13:13:43 +0700

    more bottom padding

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -6,7 +6,7 @@ interface FeatureLayoutProps {
 
 export default function FeatureLayout({ children }: FeatureLayoutProps) {
   return (
-    <div className="max-w-6xl mx-auto px-2 sm:px-4 lg:px-6 py-2">
+    <div className="max-w-6xl mx-auto px-2 sm:px-4 lg:px-6 py-2 pb-6">
       {children}
     </div>
   );
